d528344cfc9961d922a52d7aaf6723ff6094c0fe
perumal <perumal@laabamone.com>
2025-04-28 16:13:27 +0530

    changes are done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000; // You can change the port
+
+// Connect MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://pr657122:paJbbwqxoDHNWikA@cluster0.8efgpto.mongodb.net/papad"
+  )
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to API");

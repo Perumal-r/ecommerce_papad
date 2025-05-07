e9647d4e8ad3a8a29a39bb55b548bbac314d6a19
perumal <perumal@laabamone.com>
2025-05-07 18:55:34 +0530

    vercel issue is fixed in categoriesAddform

--- frontend/src/components/categoryForm/CategoriesAddForm.tsx
+++ frontend/src/components/categoryForm/CategoriesAddForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import axios from "axios";
 import toast from "react-hot-toast";
 import { fetchCategories } from "../../redux/slice/categorySlice";
 import { AppDispatch } from "../../redux/store/store";

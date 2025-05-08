f4a6a64740ecd5b411f35e95041487140ec1b6d0
perumal <perumal@laabamone.com>
2025-05-08 18:23:04 +0530

    baseUrl production changed

--- frontend/src/api/ApiClient.tsx
+++ frontend/src/api/ApiClient.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const APiClient = axios.create({
-  baseURL: "http://localhost:5000",
-    // baseURL:"https://ecommerce-papad-backend.vercel.app",
+  // baseURL: "http://localhost:5000",
+    baseURL:"https://ecommerce-papad-backend.vercel.app",
   timeout: 5000,
 });
 
